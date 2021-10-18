cc265e33005415ed303daaa9b95066976c96d142
darinamazur <allottcentr@gmail.com>
2021-10-18 17:33:33 +0300

    fill data

--- database.py
+++ database.py
@@ -8,16 +8,20 @@ class DataBase:
     def CreateTable(self):
         self.process.execute('''CREATE TABLE PROCESS
                  (ID INT PRIMARY KEY     NOT NULL,
+                 DIM    INT       NOT NULL,
                  NAME           TEXT    NOT NULL,
                  GREEN_FUNC            TEXT     NOT NULL,
                  DIFF_OPERATOR        TEXT     NOT NULL);''')
 
         self.process.close()
 
-    def Insert(self, number, name, green_func, diff_oper):
+    def Insert(self, number, dim, name, green_func, diff_oper):
         self.process = sqlite3.connect('process.db')
-        self.process.execute(f"INSERT INTO PROCESS (ID, NAME, GREEN_FUNC, DIFF_OPERATOR)\
-              VALUES ({number},'{name}', '{green_func}', '{diff_oper}')")
+        self.process.execute(f"INSERT INTO PROCESS (ID, NAME, DIM, GREEN_FUNC, DIFF_OPERATOR)\
+              VALUES ({number},{dim}, '{name}', '{green_func}', '{diff_oper}')")
 
         self.process.commit()
         self.process.close()
+
+
+
