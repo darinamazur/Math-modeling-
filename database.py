091a5f3a9255b9a9655a1109af7a75c76ecd1b9a
arnayots <ar.stoyan@gmail.com>
2021-10-18 18:21:43 +0300

    Added functions SelectGreenFunction and SelectDiffOperator

--- database.py
+++ database.py
@@ -33,8 +33,31 @@ class DataBase:
         self.process.close()
         return additional.Additional().clean_list(res)
 
+    def SelectGreenFunction(self, name, dim):
+    # returns list of strings of Green functions with apecidied NAME and DIM
+        self.process = sqlite3.connect('process.db')
+        cur = self.process.cursor()
+        cur.execute(f'SELECT GREEN_FUNC FROM PROCESS WHERE NAME = \'{name}\' AND DIM = \'{dim}\';')
+        res = cur.fetchall()
+        self.process.close()
+        return additional.Additional().clean_list(res)
+
+    def SelectDiffOperator(self, name, dim):
+    # returns list of strings of diff opearatos with apecidied NAME and DIM
+        self.process = sqlite3.connect('process.db')
+        cur = self.process.cursor()
+        cur.execute(f'SELECT DIFF_OPERATOR FROM PROCESS WHERE NAME = \'{name}\' AND DIM = \'{dim}\';')
+        res = cur.fetchall()
+        self.process.close()
+        return additional.Additional().clean_list(res)
+
+
+# ======================================================================================================================
+#                                                     Debug functions
+# ======================================================================================================================
+
     def ShowTables(self):
-        # logs to console all tables names
+    # logs to console all tables names
         self.process = sqlite3.connect('process.db')
         cur = self.process.cursor()
         cur.execute('SELECT distinct tbl_name FROM sqlite_master order by 1;')
@@ -43,7 +66,7 @@ class DataBase:
         print(res)
 
     def SelectAllTable(self, table_name):
-        # logs to console all from table table_name
+    # logs to console all from table table_name
         self.process = sqlite3.connect('process.db')
         cur = self.process.cursor()
         cur.execute(f'SELECT * FROM {table_name}')
@@ -53,7 +76,7 @@ class DataBase:
             print(row)
 
     def ShowTable(self, table_name):
-        # logs to console structure of table table_name
+    # logs to console structure of table table_name
         self.process = sqlite3.connect('process.db')
         cur = self.process.cursor()
         print(f'SELECT sql FROM sqlite_master WHERE tbl_name = \'{table_name}\' AND type = \'table\';')
